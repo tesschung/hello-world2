3d213c2f99c24b9bea364f62c41e169780326140
tesschung <geobera0910@naver.com>
2020-04-07 14:21:02 +0900

    [Add] Code From Eclipse

---
+++ javaProject/src/javaProject/Main.java
@@ -0,0 +1,9 @@
+package javaProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2");
+	}
+	
+}
---
+++ javaProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tess
+ *
+ */
+module javaProject {
+}
